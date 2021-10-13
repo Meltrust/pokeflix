c5641c80a25c82073c2c9f999347ebef7c857eb2
Amalcxc <amalcxirsi@gmail.com>
2021-10-13 19:46:11 +0300

    implamented a loop to get the id of items

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import logos from './assets/logos.png'
 import './styles/styles.css';
 
+
 const base_url = "https://pokeapi.co/api/v2/pokemon/"
 const pokemons = document.querySelector('.pokemons')
+const title = document.querySelector(".title")
 
 let pokemon1 = [];
 
@@ -10,20 +12,36 @@ async function getpokemon() {
   let response = await fetch(base_url);
   let data = await response.json();
   pokemon1 = data.results;
-  console.log(pokemon1);
 
-  pokemon1.forEach((pokemon) => {
+  pokemon1.forEach((pokemon, index) => {
     const li = document.createElement('li');
     const btn = document.createElement("button");
     btn.innerText = 'comment';
     li.innerHTML = `${pokemon.name}: ${pokemon.url}`;
     li.className = 'list_item';
+    li.id = index;
     pokemons.append(li);
-    pokemons.append(btn)
-  });
+    li.append(btn)
+    
+  }); 
+  const btn = document.querySelectorAll('button')
+  for(let i=0; i<btn.length; i++){
+    btn[i].addEventListener('click', ()=>{
+      document.querySelector('.bg-popup').style.display = "flex";
+      title.innerText = i;
+     
+    })
+  }
 }
 
+const close = document.querySelector('.close')
+
+close.addEventListener("click", () => {
+  document.querySelector('.bg-popup').style.display = "none";
+})
+
 getpokemon()
+
 
 
 
